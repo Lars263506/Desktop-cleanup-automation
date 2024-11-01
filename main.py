a6e54946f03a89981a5c3cfcda9832ea38ea15c6
Lars <263506@usn.no>
2024-11-01 20:25:07 +0100

    Changed loop for better print syntax

--- main.py
+++ main.py
@@ -4,8 +4,7 @@ def list_desktop_files():
     desktop_path = os.path.join(os.path.expanduser("~"), "Skrivebord")
     if os.path.exists(desktop_path):
         files = os.listdir(desktop_path)
-        for file in files:
-            print(file)
+        print(*files, sep="\n")
     else:
         print(f"Desktop directory does not exist: {desktop_path}")
 
