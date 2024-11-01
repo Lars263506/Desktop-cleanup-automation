f4fc309715cdd457c943a713ceb015e0ce0f81b2
Lars <263506@usn.no>
2024-11-01 20:20:56 +0100

    Main script
    For now, only contains a function to list the desktop files and prints them out. This will be changed later to return a list of the desktop files for processing by the script.

---
+++ main.py
@@ -0,0 +1,13 @@
+import os
+
+def list_desktop_files():
+    desktop_path = os.path.join(os.path.expanduser("~"), "Skrivebord")
+    if os.path.exists(desktop_path):
+        files = os.listdir(desktop_path)
+        for file in files:
+            print(file)
+    else:
+        print(f"Desktop directory does not exist: {desktop_path}")
+
+if __name__ == "__main__":
+    list_desktop_files()
