6a378878dc69c17d203172dbd8a33be88ec5b122
Lars <263506@usn.no>
2024-11-01 20:42:52 +0100

    One-time script that moves files into new folders, grouped by extensions

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import os
+from pathlib import Path
+
+desktop_path = Path(os.path.expanduser("~")) / "OneDrive/Skrivebord"
 
 def list_desktop_files():
-    desktop_path = os.path.join(os.path.expanduser("~"), "Skrivebord")
     if os.path.exists(desktop_path):
         files = os.listdir(desktop_path)
-        print(*files, sep="\n")
+        return files
     else:
         print(f"Desktop directory does not exist: {desktop_path}")
 
+def move_files():
+    files = list_desktop_files()
+
+    for file in files:
+        extension = Path(file).suffix
+        print(extension)
+        folder = desktop_path / extension
+
+        if not folder.exists():
+            folder.mkdir()
+        (desktop_path / file).rename(folder / file)
+
 if __name__ == "__main__":
-    list_desktop_files()
+    move_files()
